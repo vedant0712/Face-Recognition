64400abf59d4a60b904908f395b3de09eada49f2
vedant0712 <vedant0712@gmail.com>
2019-11-07 19:19:52 +0530

    Updates

---
+++ precache-manifest.40acc613c34339465251528bae29f1a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6543c05da56b3aca422e8f6b878093af",
+    "url": "/Face-Recognition/index.html"
+  },
+  {
+    "revision": "8ff00ae9f3119621e706",
+    "url": "/Face-Recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e8845484bf14f83f550c",
+    "url": "/Face-Recognition/static/css/main.b6eb71b0.chunk.css"
+  },
+  {
+    "revision": "8ff00ae9f3119621e706",
+    "url": "/Face-Recognition/static/js/2.3bdac705.chunk.js"
+  },
+  {
+    "revision": "e8845484bf14f83f550c",
+    "url": "/Face-Recognition/static/js/main.c93d4a98.chunk.js"
+  },
+  {
+    "revision": "105a3eaa756967e6ceb2",
+    "url": "/Face-Recognition/static/js/runtime-main.9e84b99d.js"
+  },
+  {
+    "revision": "798fbd6bc6a6f8716aaf8ea70464006e",
+    "url": "/Face-Recognition/static/media/brain.798fbd6b.png"
+  }
+]);
